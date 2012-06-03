f1e0b890b812178a57d0940def18d6c5b6f79767
Krenair <krenair@gmail.com>
2012-06-03 21:16:43 +0100

    Forgot to add quit.py

---
+++ commands/quit.py
@@ -0,0 +1,14 @@
+def run(client, line, serverhandler):
+    if line.isMoreToRead():
+        message = "Quit: " + line.readToEnd()[1:]
+    else:
+        message = "Client quit"
+
+    clientsToNotify = []
+    for channel in client.channels:
+        for member in channel.members:
+            if member not in clientsToNotify:
+                clientsToNotify.append(member)
+
+    for clientToNotify in clientsToNotify:
+        clientToNotify.writeLine(str(client) + " QUIT :" + message)
